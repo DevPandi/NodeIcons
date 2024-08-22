cbf5c2c3806834c56ed6a8017fd229150174f023
DevPandi <pandi@posteo.de>
2024-08-22 17:19:58 +0200

    Little correction.

--- Listener/Listener.php
+++ Listener/Listener.php
@@ -10,7 +10,7 @@ class Listener
 {
     public static function nodeEntityStructure(Manager $em, Structure &$structure)
     {
-        $structure->columns['pandi_own_icon'] = ['type' => Entity::BOOL, 'default' => false, 'nullable' => false];
+        $structure->columns['pandi_own_icon'] = ['type' => Entity::BOOL, 'default' => 0, 'nullable' => false];
         $structure->columns['pandi_icon'] = ['type' => Entity::STR, 'default' => '', 'nullable' => false];
         $structure->columns['pandi_icon_new'] = ['type' => Entity::STR, 'default' => '', 'nullable' => false];
     }
